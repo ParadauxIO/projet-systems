c925e80796ce334ef8db0639336d6e1bd12aecf7
Rían Errity <rian@paradaux.io>
2023-05-06 17:12:55 +0200

    Add the wildcow code which moonwalks

--- c/wildcow.c
+++ c/wildcow.c
@@ -1,3 +1,167 @@
-//
-// Created by Rian Errity on 06/05/2023.
-//
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define true 1
+
+const char* COW_FORMAT[5] = {
+        "       ^__^\n",
+        "      (%s)\\_______\n", // Eyes
+        "     (__)\\        )\\/\\\n",
+        "      %s ||----w |\n", // Tongue
+        "%s" // Variable Height
+};
+
+const char *COW_LEG_LINE[3] = {
+        "       ||     ||\n",
+        "        ||     ||\n",
+        "         ||     ||\n"
+};
+
+const int MAX_LINE_LENGTH = 100;
+
+char* repeat_string(const char* str, long N);
+void affiche_vache(char* eyes, char* tongue, long height, int frame, int x, int y);
+void update();
+void gotoxy(int x, int y);
+
+int main(int argc, char* argv[]) {
+    int option;
+    char* valeurE = "oo";
+    char* valeurT = "  ";
+    long valeurHeight = 1;
+    char* endptr; // used in str to long conversion
+    while ((option = getopt(argc, argv, "e:T:H:h")) != -1) {
+        switch (option) {
+            // Eye String
+            case 'e':
+                valeurE = optarg;
+                break;
+
+                // Tongue String
+            case 'T':
+                valeurT = optarg;
+                break;
+
+            case 'H':
+                valeurHeight = strtol(optarg, &endptr, 10);
+
+                // If there was an error converting the string
+                if (*endptr != '\0') {
+                    fprintf(stderr, "Error converting %s to a long.\n", optarg);
+                    exit(1);
+                }
+                break;
+
+                // Help / Usage Instructions.
+            case 'h':
+                printf("Usage: %s -e [v] -T [v] -h\n", argv[0]);
+                exit(0);
+                break;
+
+            default:
+                fprintf(stderr, "Usage: %s -e [v] -T [v] -h\n", argv[0]);
+                return 1;
+        }
+    }
+
+    int frame = 0;
+    int x = 50, y = 0;
+    while (true) {
+        affiche_vache(valeurE, valeurT, valeurHeight, frame, x, y);
+        usleep(200000); // Pause for 200 ms
+        update(x, y);
+        frame++;
+        if (frame > 2) {
+            frame = 0;
+        }
+        x += 1;
+        y += 1;
+    }
+}
+
+void affiche_vache(char* eyes, char* tongue, long height, int frame, int x, int y) {
+    if (strlen(eyes) != 2) {
+        fprintf(stderr, "Invalid eyes: %s. Must be exactly 2 characters.\n", eyes);
+        exit(1);
+    }
+
+    if (strlen(tongue) != 2) {
+        fprintf(stderr, "Invalid Tongue: %s. Must be exactly 2 characters.\n", tongue);
+        exit(1);
+    }
+
+    if (height < 1 || height > 10) {
+        fprintf(stderr, "Invalid Height, must be between 1 and 10\n");
+        exit(1);
+    }
+
+    if (frame > 2) {
+        fprintf(stderr, "Invalid Frame, must be between 0 and 2\n");
+        exit(1);
+    }
+
+    char* legs = repeat_string(COW_LEG_LINE[frame], height);
+    gotoxy(x, y);
+    printf("%s", COW_FORMAT[0]);
+    gotoxy(x, y+1);
+    printf(COW_FORMAT[1], eyes);
+    gotoxy(x, y+2);
+    printf("%s", COW_FORMAT[2]);
+    gotoxy(x, y+3);
+    printf(COW_FORMAT[3], tongue);
+    gotoxy(x, y+4);
+    printf(COW_FORMAT[4], legs);
+    free(legs);
+}
+
+char* repeat_string(const char* str, long N) {
+    // Allocate memory for the new string
+    int len = (int) strlen(str);
+    char* new_str = (char*) malloc((N*len + 1) * sizeof(char));
+    if (new_str == NULL) {
+        fprintf(stderr, "Error: Failed to allocate memory for new string.\n");
+        exit(1);
+    }
+
+    // Copy N copies of the original string into the new string
+    for (int i = 0; i < N; i++) {
+        strcat(new_str, str);
+    }
+
+    return new_str;
+}
+
+char* read_first_line(const char* file_path) {
+    FILE* file = fopen(file_path, "r");
+    if (file == NULL) {
+        printf("Error: unable to open the file %s\n", file_path);
+        return NULL;
+    }
+
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    read = getline(&line, &len, file);
+
+    if (read == -1) {
+        printf("Error: unable to read the first line of the file %s\n", file_path);
+        free(line);
+        fclose(file);
+        return NULL;
+    }
+
+    // Remove newline character if present
+    if (line[read - 1] == '\n') {
+        line[read - 1] = '\0';
+    }
+
+    fclose(file);
+    return line;
+}
+
+void gotoxy(int x, int y) {
+    printf("\033[%d;%dH", x, y);
+}
+void update() { printf ("\033[H\033[J") ;}
