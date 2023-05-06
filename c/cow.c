b4b4239a8344c2114c8314f2280f4abc78a5e592
Darragh <darraghnolan123@gmail.com>
2023-05-06 19:03:32 +0200

    Updated cow.c tomagachi game

---
+++ c/cow.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+
+#define LIFEROCKS 0
+#define LIFESUCKS 1
+#define BYEBYELIFE 2
+
+#define STARVING 0
+#define OVERFED 10
+
+#define MIN_STOCK 0
+#define MAX_STOCK 10
+
+
+
+int main() {
+    int cow_state = LIFEROCKS;
+    int cow_health = 5;
+    int stock = 5;
+
+    while (cow_state != BYEBYELIFE) {
+        // Draw cow health automaton
+        switch (cow_state) {
+            case LIFEROCKS:
+                printf("Liferocks (Healthy)\n");
+                //printf(COW_FORMAT, "uu", "U", 1);
+                printf("         \\   ^__^\n");
+                printf("          \\  (uu)\\_______\n");
+                printf("             (__)\\       )\\\n");
+                printf("              U  ||----w | \\__/\n");
+                printf("                 ||     ||\n");
+                break;
+
+            case LIFESUCKS:
+                printf("Lifesucks (Unwell)\n");
+                printf("         \\   ^__^\n");
+                printf("          \\  (VV)\\_______\n");
+                printf("             (__)\\       )\\\n");
+                printf("              U  ||----w | \\__/\n");
+                printf("                 ||     ||\n");
+                break;
+
+            case BYEBYELIFE:
+                printf("Byebyelife (Dead)\n");
+                printf("         \\   ^__^\n");
+                printf("          \\  (XX)\\_______\n");
+                printf("             (__)\\       )\\\n");
+                printf("              U  ||----w | \\__/\n");
+                printf("                 ||     ||\n");
+                break;
+        }
+
+        printf("Cow's health level: %d\n", cow_health);
+
+        printf("Current stock level: %d\n", stock);
+
+        int lunchfood;
+        printf("Enter amount of food to feed cow (0-%d): ", stock);
+        scanf("%d", &lunchfood);
+
+        if (lunchfood < 0 || lunchfood > stock) {
+            printf("Invalid input. Enter a value between 0 and %d.\n", stock);
+            continue;
+        }
+
+        // Generate random digestion value
+        int digestion = rand() % 4 - 3;
+
+        // Update cow's health and stock levels based on lunchfood and digestion
+        cow_health += lunchfood + digestion;
+        stock -= lunchfood;
+
+        // Generate random crop value
+        int crop = rand() % 7 - 3;
+
+        // Update stock level based on crop
+        stock += crop;
+
+        // Check if cow is overfed
+        if (cow_health >= OVERFED) {
+            cow_state = BYEBYELIFE;
+            break;
+        }
+
+        // Clamp cow's health level to STARVING and OVERFED
+        if (cow_health < STARVING) {
+            cow_health = STARVING;
+        } else if (cow_health > OVERFED) {
+            cow_health = OVERFED;
+        }
+
+        // Clamp stock level to MIN_STOCK and MAX_STOCK
+        if (stock < MIN_STOCK) {
+            stock = MIN_STOCK;
+        } else if (stock > MAX_STOCK) {
+            stock = MAX_STOCK;
+        }
+        printf("crop: %d\n", crop);
+        printf("digestion: %d\n", digestion);
+        // Check cow's health level and update state
+        if (cow_health <= STARVING) {
+            cow_state = BYEBYELIFE;
+        } else if (cow_health >= 1 && cow_health <= 3 || cow_health >= 7 && cow_health <= 9) {
+            cow_state = LIFESUCKS;
+        } else {
+            cow_state = LIFEROCKS;
+        }
+    }
+
+    printf("Your cow is dead. Game over!\n");
+
+    return 0;
+}
---
+++ cow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir c/build
+
+cmake -S ./c -B ./c/build -DCMAKE_C_COMPILER=/usr/bin/gcc
+make -C c/build
+
+./c/build/cow "$@"
